4a7cfd809b2fe3aedc49a94fbf92fea7cb8ff783
JorgeReyes <jorge.reyes.wm@gmail.com>
2025-04-02 01:10:59 -0600

    updated-gitignore-file

--- android/app/.cxx/RelWithDebInfo/3x684z67/prefab/armeabi-v7a/prefab/lib/arm-linux-androideabi/cmake/hermes-engine/hermes-engineConfig.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT TARGET hermes-engine::libhermes)
-add_library(hermes-engine::libhermes SHARED IMPORTED)
-set_target_properties(hermes-engine::libhermes PROPERTIES
-    IMPORTED_LOCATION "/Users/jorgereyes/.gradle/caches/8.12/transforms/2c8f1602dda66baaac4ecec90e68912d/transformed/hermes-android-0.78.1-release/prefab/modules/libhermes/libs/android.armeabi-v7a/libhermes.so"
-    INTERFACE_INCLUDE_DIRECTORIES "/Users/jorgereyes/.gradle/caches/8.12/transforms/2c8f1602dda66baaac4ecec90e68912d/transformed/hermes-android-0.78.1-release/prefab/modules/libhermes/include"
-    INTERFACE_LINK_LIBRARIES ""
-)
-endif()
-
